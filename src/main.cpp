e7adfa05b35d4c7af0e40799ad982ae8af285970
AkshayKumar977 <akshay.kumar_cs.aiml24@gla.ac.in>
2026-02-27 21:59:27 +0530

    implement servo movement in loop function

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+
 #include <Arduino.h>
 #include <Servo.h>
 Servo myservo; // create servo object to control a servo
@@ -8,5 +9,9 @@ void setup() {
 
 
 void loop() {
-// write your code here
+   myservo.write(0); // tell servo to go to position in variable 'pos'
+    delay(1000); // waits 1 second for the servo to reach the position
+    myservo.write(90); // tell servo to go to position in variable 'pos'
+    delay(1000); // waits 1 second for the servo to reach the position
+    myservo.write(180); // tell servo to go to position in variable 'pos'
 }
