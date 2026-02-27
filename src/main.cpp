bfa4065f93ed12c2f85f0be28124fd8eba9c5211
AkshayKumar977 <akshay.kumar_cs.aiml24@gla.ac.in>
2026-02-27 21:53:58 +0530

    attach servo to pin 9 in setup function

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 Servo myservo; // create servo object to control a servo
 void setup() {
 // write your initialization code here
-
+    myservo.attach(9); // attaches the servo on pin 9 to the servo object
 }
 
 
